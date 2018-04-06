833476cf527e83031fa157569ad3b91a902b34ea
Josh Silverman <jrsilverman71@hotmail.com>
2018-04-06 12:49:35 -0700

    started building components using react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,16 @@
-import React, { Component } from 'react';
-
-import logo from './logo.svg';
-
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Main from "./components/Main";
 import './App.css';
 
-class App extends Component {
-  state = {
-    response: ''
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.response}</p>
-      </div>
-    );
-  }
-}
+const App = () =>
+  <Router>
+    <div>
+      <Route path="/" component={Main} />
+    </div>
+  </Router>;
 
 export default App;
+
+
+
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,1 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
+import Saved from "./Saved";
+import Search from "./Search";
+
+const Main = () =>
+  <div>
+    <h1>test</h1>
+ </div>
+
+export default Main;
