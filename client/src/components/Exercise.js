1beb79f9b76c45d9a7a8a8e42a9c8cdbd0652304
Josh Silverman <jrsilverman71@hotmail.com>
2018-04-06 18:16:50 -0700

    Exercise form component

--- client/src/components/Exercise.js
+++ client/src/components/Exercise.js
@@ -2,6 +2,13 @@ import React from "react";
 
 const Exercise = () =>
   <div>
+  <form>
+  <label>
+    Exercise:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
 
   </div>
 
