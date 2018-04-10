108d3265d66f34a6a6d5503240cc76f0ab351815
Josh Silverman <jrsilverman71@hotmail.com>
2018-04-10 09:42:05 -0700

    fixed schema

--- models/workout.js
+++ models/workout.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
+const workoutSchema = new Schema({
   exercise: { type: String, required: true },
   date: { type: Date, default: Date.now },
   weight: { type: String, required: true },
